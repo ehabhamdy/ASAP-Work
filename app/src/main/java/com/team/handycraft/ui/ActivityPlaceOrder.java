503bfbdb9a919d24792c45d3a6565bf99c839b23
ehabhamdy <ehabhamdy2012@gmail.com>
2016-06-30 15:39:47 +0200

    creating place order activity

---
+++ app/src/main/java/com/team/handycraft/ui/ActivityPlaceOrder.java
@@ -0,0 +1,15 @@
+package com.team.handycraft.ui;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+/**
+ * Created by Ehab on 6/30/16.
+ */
+public class ActivityPlaceOrder extends ActivityBase {
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        
+    }
+}
